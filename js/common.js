31cc757c9002cbf2276a709810fc21d8e0470917
xh <1637540706@qq.com>
2023-10-12 15:55:30 +0800

    第一次

---
+++ js/common.js
@@ -0,0 +1,37 @@
+var tyUrl = 'http://192.168.0.128:8080/MoveOffice/';
+var fileUrl = 'http://192.168.0.128:8080/MoveOffice/';
+var zpUrl = 'http://192.168.0.128:8080/MoveOffice/';
+/*var tyUrl = 'http://117.60.146.107/MoveOffice/';
+var fileUrl = 'http://117.60.146.107/MoveOffice/';
+var zpUrl = 'http://117.60.146.107/MoveOffice/';*/
+
+
+function convert_json(obj) {
+	var result = JSON.stringify(obj);
+	result = result.replace("[", "");
+	result = result.replace("]", "");
+	console.log("result"+result);
+	if(strIsNullOrEmpty(result)){
+		return "";
+	}else{
+		return JSON.parse(result);
+	}
+}
+
+
+function strIsNullOrEmpty(strVal) {
+	if (strVal == '' || strVal == null || strVal == undefined) {
+		return true;
+	} else { 
+		return false;
+	}
+}
+
+
+function listIsNullOrEmpty(obj){
+	if(JSON.stringify(obj).length>2){
+		return true;
+	}else{
+		return false;
+	}
+}
---
+++ js/loginPage.js
@@ -0,0 +1,20 @@
+var form = layui.form, layer = layui.layer;
+$(function(){	
+	
+})
+
+
+
+window.onload = function (){
+	/*var retstr=document.getElementById("retstr").value;
+	if(retstr!=""){
+		if(retstr == "请使用强密码！"){
+			var regExp=	/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/;
+			layer.msg('您的密码过于简单，请修改密码！', {icon: 7,time: 2000},function(){
+				
+			}); 
+		}else{
+			alert(retstr);
+		}
+	}*/
+}
